53328cad44dc4a18430d6bbc2daa16d52f0f6b6e
Misha-Belov <59139007+Misha-Belov@users.noreply.github.com>
2025-03-04 15:56:41 +0300

    segfault for empty removed

--- main.cpp
+++ main.cpp
@@ -1,18 +1,46 @@
 #include <vector>
 #include <iostream>
+#include <assert.h>
+
 #include "avl_tree.h"
 
 using namespace avl_tree;
 
 int main()
 {
-    avl_tree_t<int> t;
+    // avl_tree_t<int> t;
+    avl_tree_t<int> T;
 
-    std::vector<int> data{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for (int i = 0; i < data.size(); i++)
-        t.insert_key(data[i]);
+    // std::vector<int> data{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // for (int i = 0; i < data.size(); i++)
+    //     t.insert_key(data[i]);
 
-    t.print(std::cout);
+    // t.print(std::cout);
+    // t.print_interval(2, 8);
 
+    while (std::cin.good())
+    {
+        char command = '\0';
+        std::cin >> command;
+
+        if (command == 'k') {
+            int key = 0;
+            std::cin >> key;
+
+            T.insert_key(key);
+        } else if (command == 'q') {
+            int val1 = 0, val2 = 0;
+            std::cin >> val1 >> val2;
+
+            assert(val1 <= val2);
+
+            T.print_interval(val1, val2);
+        } else {
+            std::cout << "wrong request \n";
+            break;
+        }
+    }
+    
+    T.print(std::cout);
     return 0;
 }
